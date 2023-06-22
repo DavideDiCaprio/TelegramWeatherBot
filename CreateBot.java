0f1a96e366c2ebaa4417c63bf18ebe76228b5772
Davide <93427056+DavideDiCaprio@users.noreply.github.com>
2023-06-22 16:24:22 +0200

    Create CreateBot.java

---
+++ CreateBot.java
@@ -0,0 +1,45 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class CreateBot extends TelegramLongPollingBot {
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+        System.out.println(update.getMessage().getText());
+        System.out.println(update.getMessage().getFrom().getFirstName());
+
+
+        String command = update.getMessage().getText();
+
+        //command must be created in telegram
+
+        if (command.equals("/run")) {
+            String message = "Run Forrest";
+            SendMessage response = new SendMessage();
+            response.setChatId(update.getMessage().getChatId().toString());
+            response.setText(message);
+
+            try {
+                execute(response);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        //
+        return "ddcmybot";
+    }
+
+    @Override
+    public String getBotToken() {
+        //
+        return "6076900482:AAEubi8o775ttgfQbkdTSAA4806eP45T9jA";
+    }
+    
+}
